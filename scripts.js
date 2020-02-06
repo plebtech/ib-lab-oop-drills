e8934acb7d1fd2a80078389418fac6661696a93a
Nick Mc <nbmc@protonmail.com>
2020-02-06 15:18:05 -0600

    completed 'object literals, pseudo classes and methods' section.

--- scripts.js
+++ scripts.js
@@ -33,15 +33,25 @@
 // person3.sayHello();
 // person4.sayHello();
 // person5.sayHello();
-function Person(name, city, age) {
-    this.name = name;
-    this.city = city;
-    this.age = age;
-}
-Person.prototype.sayHello = function(name, city, age) {
-    console.log(`Hey! My name is ${this.name}. I am ${this.age} years old and live in ${this.city}.`);
-}
+// function Person(name, city, age) {
+//     this.name = name;
+//     this.city = city;
+//     this.age = age;
+// }
+// Person.prototype.sayHello = function(name, city, age) {
+//     console.log(`Hey! My name is ${this.name}. I am ${this.age} years old and live in ${this.city}.`);
+// }
 
+class Person {
+    constructor(name, city, age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+    sayHello() {
+        console.log(`Hey! My name is ${this.name}. I am ${this.age} years old and live in ${this.city}.`);
+    }
+}
 let person1 = new Person('Isabel', 'NSW', 19);
 person1.sayHello();
 let person2 = new Person('Justin', 'Birmingham', 36);
